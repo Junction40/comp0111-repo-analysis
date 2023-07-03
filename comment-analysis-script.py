04f27c21249b5afefdb3449e6acb1bb1051487e5
Dave Williams <davejjwilliams@gmail.com>
2023-07-03 23:55:03 +0100

    Get Pull Requests & PR Comments

--- comment-analysis-script.py
+++ comment-analysis-script.py
@@ -2,8 +2,13 @@
 from github import Github
 from github import Auth
 
+# Import CSV Package
+import csv
+
 # Import config.py
 import config
+
+analysed_repository = "PyGithub/PyGithub"
 
 # Define auth using access token from config.py
 auth = Auth.Token(config.github_access_token)
@@ -15,6 +20,17 @@ g = Github(auth=auth)
 # # Define Github Enterprise object
 # g = Github(auth=auth, base_url="https://{hostname}/api/v3")
 
-# Temporary test: print your repos
-for repo in g.get_user().get_repos():
-    print(repo.name)
+# # Temporary test: print your repos
+# for repo in g.get_user().get_repos():
+#     print(repo.name)
+
+# Fetch specific repository
+repo = g.get_repo(analysed_repository)
+
+# Get pull requests
+pull_requests = repo.get_pulls()
+
+# Get all comments from all pull requests
+for pr in pull_requests:
+    for comment in pr.get_comments():
+        print(comment)
