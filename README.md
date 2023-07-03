b38dd102c3d802f3514bcc2ae85ac9156b2567af
Dave Williams <davejjwilliams@gmail.com>
2023-07-03 19:50:22 +0100

    Repo Setup, Dependencies, README Instructions

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# comp0111-repo-analysis
+# Repo Analyser
+
+This script will be used to analyse repositories where
+
+# Setup
+
+1. Install the dependencies: `pip install -r requirements.txt`
+
+2. Create a Github access token to use PyGithub.
+
+3. Create a `config.py` file in the root directory and add a variable with a string value of the access token you've created:
+
+#### **`config.py`**
+
+```py
+github_access_token = *YOUR_ACCESS_TOKEN*
+```
--- comment-analysis-script.py
+++ comment-analysis-script.py
@@ -1,1 +1,20 @@
 # Import PyGithub
+from github import Github
+from github import Auth
+
+# Import config.py
+import config
+
+# Define auth using access token from config.py
+auth = Auth.Token(config.github_access_token)
+
+# Define Github object
+g = Github(auth=auth)
+
+# # ** Uncomment the following 2 lines and comment the 2 lines above when testing on BBGithub. **
+# # Define Github Enterprise object
+# g = Github(auth=auth, base_url="https://{hostname}/api/v3")
+
+# Temporary test: print your repos
+for repo in g.get_user().get_repos():
+    print(repo.name)
