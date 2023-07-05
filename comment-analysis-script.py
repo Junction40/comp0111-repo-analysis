af839e369e28429bdcd4b26c5c1b1c570de76419
Dave Williams <davejjwilliams@gmail.com>
2023-07-05 16:06:26 +0100

    Test Adding Header to CSV Results File

--- comment-analysis-script.py
+++ comment-analysis-script.py
@@ -2,13 +2,28 @@
 from github import Github
 from github import Auth
 
+SUGGEST_START = "```suggestion"
+SUGGEST_END = "```"
+
 # Import CSV Package
 import csv
 
 # Import config.py
 import config
 
+# *CHANGE VALUE TO NAME OF REPOSITORY TO ANALYSE*
 analysed_repository = "PyGithub/PyGithub"
+
+# Filename to store analysis results
+results_filename = "results/results_{}.csv".format(analysed_repository.split("/")[1])
+
+# Header for CSV results file
+header = ["repo", "prID", "comment"]
+
+# Add header row to the results file
+with open(results_filename, "w") as f:
+    writer = csv.writer(f, delimiter=",")
+    writer.writerow(header)
 
 # Define auth using access token from config.py
 auth = Auth.Token(config.github_access_token)
@@ -19,10 +34,6 @@ g = Github(auth=auth)
 # # ** Uncomment the following 2 lines and comment the 2 lines above when testing on BBGithub. **
 # # Define Github Enterprise object
 # g = Github(auth=auth, base_url="https://{hostname}/api/v3")
-
-# # Temporary test: print your repos
-# for repo in g.get_user().get_repos():
-#     print(repo.name)
 
 # Fetch specific repository
 repo = g.get_repo(analysed_repository)
